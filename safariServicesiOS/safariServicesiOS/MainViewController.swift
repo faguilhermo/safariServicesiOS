e9283b1e0e0c465fc0b9017174ce5ad4360e09e5
Fabrício <guilhermo.fabricio@gmail.com>
2020-03-31 15:43:20 -0300

    Initial commit

---
+++ safariServicesiOS/safariServicesiOS/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  safariServicesiOS
+//
+//  Created by Fabrício Guilhermo on 31/03/20.
+//  Copyright © 2020 Fabrício Guilhermo. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
+    }
+}
